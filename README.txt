d96465603c332851ddd7e3d577710b1caef8ba44
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-03-28 17:25:46 +0200

    Wavefront Propagation: integration of SRW Sources in OASYS

---
+++ README.txt
@@ -0,0 +1,17 @@
+WOFRY - SRW (Wave Optics FRamework in pYthon) for WISE library
+======
+
+Kernel classes for WOFRY - WISE in python
+
+syned package requires Python 3.3 or newer.
+
+
+Installing
+----------
+
+syned is pip installable (https://pip.pypa.io/), simply run:
+
+    pip install wofrywise
+
+to install it.
+
---
+++ wofrywise/propagator/wavefront1D/wise_wavefront.py
@@ -0,0 +1,30 @@
+import numpy
+
+from wofry.propagator.wavefront1D.generic_wavefront import GenericWavefront1D
+from wofry.propagator.decorators import WavefrontDecorator
+
+class WiseWavefront(WavefrontDecorator):
+
+    def __init__(self,
+                 wavelength=1e-10,
+                 positions=numpy.zeros(100),
+                 electric_fields=numpy.zeros(100),
+                 residuals=numpy.zeros(0)):
+        self._wavelength = wavelength
+        self._positions = positions
+        self._electric_fields = electric_fields
+        self._residuals = residuals
+
+
+    def toGenericWavefront(self):
+        return GenericWavefront1D.initialize_wavefront_from_arrays(x_array=self._positions_s, y_array=self._electric_fields, wavelength=self._wavelength)
+
+    @classmethod
+    def fromGenericWavefront(cls, wavefront):
+        wavelength = wavefront.get_wavelength()
+        position = wavefront.get_abscissas()
+        electric_fields = wavefront.get_complex_amplitude()
+
+        return WiseWavefront(wavelength=wavelength,
+                             position=position,
+                             electric_fields=electric_fields)
